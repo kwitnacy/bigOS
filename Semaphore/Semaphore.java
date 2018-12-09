94d31ed0547b683453a9cc2c740a63858a45823c
Evenlaxxus <45331752+Evenlaxxus@users.noreply.github.com>
2018-12-09 19:38:21 +0100

    semafor

---
+++ Semaphore/Semaphore.java
@@ -0,0 +1,26 @@
+import java.util.Queue;
+
+public class Semaphore{
+    Queue<Integer> queue;
+    int value;
+
+    Semaphore(int val){
+        this.value = val;
+    }
+
+    public void wait_s(){
+        this.value--;
+        if(this.value < 0){
+            //add to this.process_queue
+            //block();
+        }
+    }
+
+    public void signal_s(){
+        this.value++;
+        if(this.value <= 0){
+            //Process P = process_queue.poll();
+            //wakeup(P);
+        }
+    }
+}
