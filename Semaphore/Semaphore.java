9fd3146555c198f0191ddc92d70928dd4333e59a
Evenlaxxus <r.ewiak1404@wp.pl>
2018-12-20 12:56:55 +0100

    Update semaphore

--- Semaphore/Semaphore.java
+++ Semaphore/Semaphore.java
@@ -1,28 +1,31 @@
 package Semaphore;
 
+import Procesy.Process_container;
+import Procesy.State;
+
 import java.util.Queue;
 
 public class Semaphore{
-    Queue<Process> process_queue;
-    int value;
+    private Queue<Integer> process_queue;
+    private int value;
 
     public Semaphore(int val){
         this.value = val;
     }
 
-    public void wait_s(){
+    public void wait_s(int pid){
         this.value--;
         if(this.value < 0){
-            //add to this.process_queue
-            //block();
+            process_queue.offer(pid);
+            Process_container.get_by_PID(pid).change_state(State.Waiting);
         }
     }
 
     public void signal_s(){
         this.value++;
         if(this.value <= 0){
-            Process P = process_queue.poll();
-            //wakeup(P);
+            int pid = process_queue.poll();
+            Process_container.get_by_PID(pid).change_state(State.Ready);
         }
     }
 }
