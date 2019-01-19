6057c1564c41570ffb69c3ce1aacafc1b7ed87e6
Evenlaxxus <r.ewiak1404@wp.pl>
2019-01-19 12:36:44 +0100

    Zmiana typu kolejki na ArrayDeque i dodanie print_queue

--- Semaphore/Semaphore.java
+++ Semaphore/Semaphore.java
@@ -3,9 +3,9 @@ package Semaphore;
 import Procesy.Process_container;
 import Procesy.State;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.Queue;
+import java.util.ArrayDeque;
+
 
 public class Semaphore{
     private Queue<Integer> process_queue;
@@ -14,101 +14,17 @@ public class Semaphore{
 
     public Semaphore(int val){
         this.value = val;
-        this.process_queue = new Queue<Integer>() {
-            @Override
-            public boolean add(Integer integer) {
-                return false;
-            }
-
-            @Override
-            public boolean offer(Integer integer) {
-                return false;
-            }
-
-            @Override
-            public Integer remove() {
-                return null;
-            }
-
-            @Override
-            public Integer poll() {
-                return null;
-            }
-
-            @Override
-            public Integer element() {
-                return null;
-            }
-
-            @Override
-            public Integer peek() {
-                return null;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<Integer> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends Integer> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-        };
+        this.process_queue = new ArrayDeque<>();
     }
 
     public int getValue() {
         return value;
+    }
+
+    public void print_queue(){
+        for(Integer s : this.process_queue) {
+            System.out.println(s);
+        }
     }
 
     public void wait_s(int pid){
